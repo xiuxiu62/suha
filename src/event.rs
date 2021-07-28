0721c1358ee700b79a4ea56e922a4f7fcdf39dbc
justincremer <jacremer@live.com>
2021-07-27 17:06:46 -0700

    cleans up the event listener threads and fixes blocking issue

--- src/event.rs
+++ src/event.rs
@@ -1,69 +1,77 @@
 use std::io;
-use std::sync::mpsc::{Receiver, Sender};
-use std::time::{Duration, Instant};
-use std::{sync::mpsc, thread};
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 
-use termion::event::Event;
+use termion::event::Key;
 use termion::input::TermRead;
 
-const TICK_RATE: Duration = Duration::from_millis(200);
-
-#[derive(Debug, Clone)]
-pub enum RuntimeEvent {
-    Termion(Event),
-    Signal(i32),
+pub enum Event<I> {
+    Input(I),
     Tick,
 }
 
-#[derive(Debug)]
-pub struct EventHandler {
-    pub event_tx: Sender<RuntimeEvent>,
-    pub event_rx: Receiver<RuntimeEvent>,
+#[derive(Debug, Clone, Copy)]
+pub struct Config {
+    pub tick_rate: Duration,
 }
 
-impl EventHandler {
-    pub fn new() -> Self {
-        let (event_tx, event_rx) = mpsc::channel::<RuntimeEvent>();
-        Self { event_tx, event_rx }
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            tick_rate: Duration::from_millis(250),
+        }
     }
+}
 
-    pub fn spawn() {
-        thread::spawn(move || {
-            let worker = Self::new();
-            let mut last_tick = Instant::now();
+pub struct Events {
+    rx: mpsc::Receiver<Event<Key>>,
+    input_handle: thread::JoinHandle<()>,
+    tick_handle: thread::JoinHandle<()>,
+}
 
-            loop {
-                let timeout = TICK_RATE
-                    .checked_sub(last_tick.elapsed())
-                    .unwrap_or_else(|| Duration::from_secs(0));
+impl Events {
+    pub fn new(config: Option<Config>) -> Self {
+        let config = if let Some(conf) = config {
+            conf
+        } else {
+            Config::default()
+        };
 
+        let (tx, rx) = mpsc::channel();
+        let input_handle = {
+            let tx = tx.clone();
+            thread::spawn(move || {
                 let stdin = io::stdin();
-                for c in stdin.keys() {
-                    worker
-                        .event_tx
-                        .send(RuntimeEvent::Termion(Event::Key(c.unwrap())))
-                        .expect("can send events");
-                }
-
-                if last_tick.elapsed() >= TICK_RATE {
-                    if let Ok(_) = worker.event_tx.send(RuntimeEvent::Tick) {
-                        last_tick = Instant::now();
+                for event in stdin.keys() {
+                    if let Ok(key) = event {
+                        if let Err(err) = tx.send(Event::Input(key)) {
+                            eprintln!("{}", err);
+                            return;
+                        }
                     }
                 }
-            }
-        });
+            })
+        };
+
+        let tick_handle = {
+            thread::spawn(move || loop {
+                if let Err(err) = tx.send(Event::Tick) {
+                    eprintln!("{}", err);
+                    break;
+                }
+                thread::sleep(config.tick_rate);
+            })
+        };
+
+        Self {
+            rx,
+            input_handle,
+            tick_handle,
+        }
+    }
+
+    pub fn next(&self) -> Result<Event<Key>, mpsc::RecvError> {
+        self.rx.recv()
     }
 }
-
-// fn handle_key_event(rx: &Receiver<KeyEvent>) {
-// 	match rx.recv()? {
-// 		                    Key::Char('q') => tx.send(KeyEvent::Exit(0)).expect("can send events"),
-//                     Key::Ctrl(c) => {
-//                         if c == 'c' || c == 'C' {
-//                             tx.send(KeyEvent::Exit(0)).expect("can send events")
-//                         }
-//                     }
-//                     _ => {}
-// 		KeyEvent::Exit(code) =>
-// 	}
-// }
