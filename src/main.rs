faa7d38da49a43fd29c0adfa1b8127f7c2e49463
xiuxiu62 <jacremer@live.com>
2021-08-24 15:37:00 -0700

    better clean up in main

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,14 @@ use structopt::{clap::Shell, StructOpt};
 use run::*;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "suha")]
+#[structopt(name = "suha", about = "A cross platform terminal file manager.")]
 pub struct Opt {
     #[structopt(parse(from_os_str))]
     file: Option<PathBuf>,
 }
 
 const FPS: u64 = 120;
+const DEFAULT_PATH: &'static str = "/home/xiuxiu";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -28,16 +29,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Opt::clap().gen_completions(env!("CARGO_PKG_NAME"), Shell::Bash, "target");
 
     let opts = Opt::from_args();
-    match opts.file {
-        Some(path) => {
-            let mut stdout = setup()?;
-            if let Err(e) = run(&mut stdout, path.as_path(), FPS).await {
-                eprintln!("{}", e)
-            };
+    let path: PathBuf = match opts.file {
+        Some(path) => path,
+        None => PathBuf::from(DEFAULT_PATH),
+    };
+
+    let mut stdout = setup()?;
+    match run(&mut stdout, path.as_path(), FPS).await {
+        Ok(()) => cleanup(&mut stdout)?,
+        Err(e) => {
             cleanup(&mut stdout)?;
+            eprintln!("{}", e)
         }
-        None => eprintln!("\nplease provide a valid file path"),
-    }
+    };
 
     Ok(())
 }
