e290809d761ca6ba7bd969705a01b0e360f15a22
xiuxiu62 <jacremer@live.com>
2021-08-23 20:23:42 -0700

    adds async event handling

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,39 @@
 #![allow(dead_code)]
 
-// mod event;
+mod event;
 mod fs;
 mod option;
 // mod ui;
 
-use std::env;
-use std::io::Stdout;
+use std::io::{self, Stdout};
+use std::path::Path;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
-use std::{io, path::Path, thread};
+use std::{env, thread};
 
-use crossbeam_channel::Receiver;
-use crossterm::cursor::{self, position};
-use crossterm::event::{Event, EventStream, KeyCode};
+use crossterm::cursor;
 use crossterm::{execute, terminal};
-use futures::{future::FutureExt, select, StreamExt};
-use futures_timer::Delay;
 use tui::layout::{Constraint, Direction, Layout};
 use tui::Terminal;
 use tui::{backend::CrosstermBackend, widgets::Paragraph};
 
+use crate::event::{Command, Worker};
 use crate::fs::Cache;
 use crate::option::DisplayOptions;
 
-const HELP: &'static str = r#"help"#;
-
-pub struct Listener(Receiver<Event>);
-
-impl Listener {
-    pub fn new() -> Self {
-        let (tx, rx) = crossbeam_channel::unbounded();
-        thread::spawn(move || loop {
-            tx.send(crossterm::event::read().unwrap()).unwrap();
-        });
-        Self(rx)
-    }
-
-    pub fn get(&self) -> &Receiver<Event> {
-        &self.0
-    }
-}
-
-async fn print_events() {
-    let mut reader = EventStream::new();
-
-    loop {
-        let mut delay = Delay::new(Duration::from_millis(1_000)).fuse();
-        let mut event = reader.next().fuse();
-
-        select! {
-            _ = delay => {},
-            maybe_event = event => {
-                match maybe_event {
-                    Some(Ok(event)) => {
-                        println!("Event::{:?}\r", event);
-
-                        if event == Event::Key(KeyCode::Char('c').into()) {
-                            println!("Cursor position: {:?}\r", position());
-                        }
-
-                        if event == Event::Key(KeyCode::Esc.into()) {
-                            break;
-                        }
-                    },
-                    Some(Err(e)) => println!("Error: {:?}\r", e),
-                    None => break,
-                }
-            }
-        };
-    }
-}
+const HELP: &'static str = /* @MANSTART{suha} */
+    r#"
+NAME
+    suha - a file manager
+SYNOPSIS
+    suha [ -h | --help ]
+DESCRIPTION
+    suha is a cross platform terminal file manager
+OPTIONS
+    -h
+    --help
+        Print this help page.
+"#; /* @MANEND */
 
 pub fn setup() -> crossterm::Result<io::Stdout> {
     let mut stdout = io::stdout();
@@ -91,57 +54,68 @@ pub fn cleanup(stdout: &mut Stdout) -> crossterm::Result<()> {
     Ok(())
 }
 
-fn try_run(stdout: &mut Stdout, path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+async fn try_run(stdout: &mut Stdout, path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let session_cache = Arc::from(Mutex::from(Cache::new()));
     let options = DisplayOptions::new(false, false);
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
+    let event_worker = Worker::new();
 
     session_cache
         .lock()
         .unwrap()
         .populate_to_root(path, &options)?;
 
-    terminal.draw(|frame| {
-        let body = session_cache
-            .lock()
-            .unwrap()
-            .inner
-            .get(path)
-            .unwrap()
-            .to_string();
+    loop {
+        if let Some(command) = event_worker.handle().await {
+            match command {
+                Command::Exit => break,
+                Command::Debug(s) => println!("{}", s),
+                _ => {}
+            }
+        };
 
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([Constraint::Percentage(100)].as_ref())
-            .split(frame.size());
+        terminal.draw(|frame| {
+            let body = session_cache
+                .lock()
+                .unwrap()
+                .inner
+                .get(path)
+                .unwrap()
+                .to_string();
 
-        frame.render_widget(Paragraph::new(body), chunks[0]);
-    })?;
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([Constraint::Percentage(100)].as_ref())
+                .split(frame.size());
+
+            frame.render_widget(Paragraph::new(body), chunks[0]);
+        })?;
+
+        // draw 50 frames / second
+        thread::sleep(Duration::from_millis(1_000 / 50));
+    }
 
     Ok(())
 }
 
-#[tokio::main]
+#[tokio::main] // TODO: replace with structopt
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut stdout = setup()?;
     let args: Vec<String> = env::args().collect();
-
-    // TODO: replace with structopt
-    if let 2 = args.len() {
-        match args[1].as_str() {
-            "-h" | "--help" => println!("{}", HELP),
-            s => {
-                try_run(&mut stdout, Path::new(s))?;
-
-                print_events().await;
-
-                cleanup(&mut stdout)?;
-                return Ok(());
-            }
+    match args.len() {
+        2 => {
+            let mut stdout = setup()?;
+            match args[1].as_str() {
+                "-h" | "--help" => println!("{}", HELP),
+                path => {
+                    if let Err(e) = try_run(&mut stdout, Path::new(path)).await {
+                        eprintln!("{}", e)
+                    }
+                }
+            };
+            cleanup(&mut stdout)?;
         }
-    } else {
-        eprintln!("please provide a valid file path");
+        _ => eprintln!("please provide a valid file path"),
     }
 
     Ok(())
